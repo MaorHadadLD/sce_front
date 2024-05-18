fdcdf47e67827e1afbfbd0ad149d8ae979bcad1d
MaorHadadLD <maorhadad94@gmail.com>
2024-05-18 10:47:32 +0300

    student list

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 import React from 'react';
-import StudentListRow from './components/StudentListRow';
+import StudentList from './components/StudentList';
 
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <StudentListRow name="John Doe" id="123456" imagUrl="./assets/avatar_user.png"/>
+      <StudentList />
       </View>
   );
 
---
+++ components/StudentList.tsx
@@ -0,0 +1,13 @@
+import { FC, useState } from "react";
+import { Text } from "react-native";
+
+
+
+const StudentList: FC = () => {
+    return (
+        <Text>Student List</Text>
+    );
+}
+
+
+export default StudentList;
