1b1fe03b677140ff54643b90e31b4e073bdb7e90
MaorHadadLD <maorhadad94@gmail.com>
2024-05-18 10:51:04 +0300

    student data

--- components/StudentList.tsx
+++ components/StudentList.tsx
@@ -1,7 +1,17 @@
 import { FC, useState } from "react";
 import { Text } from "react-native";
 
+type Student = {
+    name: string;
+    id: string;
+    imgUrl: string;
+}
 
+const data: Student[] = [
+    {name: 'John Doe', id: '123', imgUrl: 'https://via.placeholder.com/150'},
+    {name: 'Jane Doe', id: '456', imgUrl: 'https://via.placeholder.com/150' },
+    {name: 'John Smith', id: '789', imgUrl: 'https://via.placeholder.com/150'},
+]
 
 const StudentList: FC = () => {
     return (
