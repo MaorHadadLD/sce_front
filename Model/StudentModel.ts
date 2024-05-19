698eee32577b999d557a1618938f8630dd7e1190
MaorHadadLD <maorhadad94@gmail.com>
2024-05-19 15:12:32 +0300

    update student model

--- Model/StudentModel.ts
+++ Model/StudentModel.ts
@@ -1,32 +1,29 @@
 export type Student = {
-    name: string;
-    id: string;
-    imgUrl: string;
+    name: string,
+    id: string,
+    imgUrl: string
 }
 
 const data: Student[] = [
-    {name: 'John Doe', id: '123', imgUrl: 'https://via.placeholder.com/150'},
-    {name: 'Jane Doe', id: '456', imgUrl: 'https://via.placeholder.com/150' },
-    {name: 'John Smith', id: '789', imgUrl: 'https://via.placeholder.com/150'},
-]
+];
 
 const getAllStudents = (): Student[] => {
     return data;
 }
 
 const getStudent = (id: string): Student | undefined => {
-    return data.find((student) => student.id === id);
+    return data.find((student) => student.id == id);
 }
 
 const addStudent = (student: Student) => {
     data.push(student);
 }
 
-const deletStudent = (id: string) => {
+const deleteStudent = (id: string) => {
     const index = data.findIndex((student) => student.id === id);
-    if(index !== -1) {
+    if (index !== -1) {
         data.splice(index, 1);
     }
 }
 
-export default {getAllStudents, getStudent, addStudent, deletStudent};
+export default { getAllStudents, getStudent, addStudent, deleteStudent };
