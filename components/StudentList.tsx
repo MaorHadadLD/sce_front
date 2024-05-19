1e7ce24352cee1820a6aa49ffd439916c7e5ec96
MaorHadadLD <maorhadad94@gmail.com>
2024-05-19 12:15:30 +0300

    student model

--- components/StudentList.tsx
+++ components/StudentList.tsx
@@ -2,17 +2,7 @@ import { FC, useState } from "react";
 import { FlatList, Text, StyleSheet} from "react-native";
 import StudentListRow from "./StudentListRow";
 
-type Student = {
-    name: string;
-    id: string;
-    imgUrl: string;
-}
 
-const data: Student[] = [
-    {name: 'John Doe', id: '123', imgUrl: 'https://via.placeholder.com/150'},
-    {name: 'Jane Doe', id: '456', imgUrl: 'https://via.placeholder.com/150' },
-    {name: 'John Smith', id: '789', imgUrl: 'https://via.placeholder.com/150'},
-]
 
 const StudentList: FC = () => {
     return (
