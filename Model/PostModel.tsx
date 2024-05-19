5165c9b9b93e97173c3b34b7dc7f2ba03c3fee59
MaorHadadLD <maorhadad94@gmail.com>
2024-05-19 17:45:59 +0300

    add get all posts

--- Model/PostModel.tsx
+++ Model/PostModel.tsx
@@ -1,3 +1,5 @@
+import PostApi from '../api/PostApi'
+
 export type Post = {
     title: string,
     message: string,
@@ -7,3 +9,21 @@ export type Post = {
 const data: Post[] = [
 ];
 
+
+const getAllPosts = async () => {
+    console.log("getAllPosts()" )
+    const res: any = await PostApi.getAllPosts ()
+    let data = Array<Post>()
+    if (res.data) {
+    res.data.forEach((obj: any) => {
+    console.log("element: " + obj._id)
+    const st: Post = {
+    title: obj.title,
+    message: obj.message,
+    owner: obj.owner
+    }
+    data.push(st)
+    });
+    }
+    return data
+   }
