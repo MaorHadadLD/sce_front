25431a959881da070dee1c5375fe016ba01aede6
MaorHadadLD <maorhadad94@gmail.com>
2024-05-25 21:58:35 +0300

    updating post list row

--- components/PostListRow.tsx
+++ components/PostListRow.tsx
@@ -1,5 +1,8 @@
 import { StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import UserModel from '../Model/UserModel';
+import StudentApi from '../api/StudentApi';
 
 const PostListRow: FC<{
     title: string,
@@ -13,6 +16,30 @@ const PostListRow: FC<{
         onItemSelected({ _id, title, message, owner, imgUrl });
     }
 
+    const [data, setData] = useState<any>();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+       const fetchUserData = async () => {
+          try {
+           
+            const user: any = await StudentApi.getStudentById(owner);
+           
+            if (user) { 
+              
+              setData(user.data);
+             
+              setLoading(false);
+            }
+          } catch (error) {
+            console.error('Failed to fetch user:', error);
+            setLoading(true);
+          }
+        };
+        fetchUserData();
+        }, [data]); 
+     
+      
     return (
         <TouchableHighlight onPress={onPress} underlayColor={'#DDDDDD'}>
             <View style={styles.container}>
@@ -21,7 +48,7 @@ const PostListRow: FC<{
                     <Text style={styles.owner}>{owner}</Text>
                 </View>
                 <Image style={styles.image} source={{ uri: imgUrl }} />
-                <View style={styles.content}>
+                <View style={styles.content}> 
                     <Text style={styles.title}>{title}</Text>
                     <Text style={styles.message}>{message}</Text>
                 </View>
