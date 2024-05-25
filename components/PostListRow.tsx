c08bc9c7f83635390d53f7d039fb5e1ccfe9555c
MaorHadadLD <maorhadad94@gmail.com>
2024-05-25 19:27:40 +0300

    update post style

--- components/PostListRow.tsx
+++ components/PostListRow.tsx
@@ -1,63 +1,73 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity, Button, Alert, TextInput, StatusBar, TouchableHighlight } from 'react-native';
-import React, { useState, FC } from 'react';
-
-
+import { StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
+import React, { FC } from 'react';
 
 const PostListRow: FC<{
     title: string,
     message: string,
-    owner: string
-    imgUrl: string
-    _id: string
-    onItemSelected: (_id: string) => void
-}> = ({ title,message, owner, imgUrl, _id ,onItemSelected }) => {
+    owner: string,
+    imgUrl: string,
+    _id: string,
+    onItemSelected: (post: { _id: string, title: string, message: string, owner: string, imgUrl: string }) => void
+}> = ({ title, message, owner, imgUrl, _id, onItemSelected }) => {
     const onPress = () => {
-        console.log('Item selected2222: ' + _id);
-        onItemSelected(_id);
+        onItemSelected({ _id, title, message, owner, imgUrl });
     }
+
     return (
-        <TouchableHighlight
-            onPress={onPress}
-            underlayColor={'grey'}>
-            <View style={styles.listrow}>
-                <Image style={styles.avatar} source={require('../assets/avatar_user.png')} />
-                <View style={styles.info}>
-                    <Text style={styles.name}>{title}</Text>
-                    <Text style={styles.id}>{message}</Text>
-                    <Image style={styles.avatar} source={{uri: imgUrl}} />
-                    <Text style={styles.name}>{owner}</Text>
+        <TouchableHighlight onPress={onPress} underlayColor={'#DDDDDD'}>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Image style={styles.avatar} source={require('../assets/avatar_user.png')} />
+                    <Text style={styles.owner}>{owner}</Text>
+                </View>
+                <Image style={styles.image} source={{ uri: imgUrl }} />
+                <View style={styles.content}>
+                    <Text style={styles.title}>{title}</Text>
+                    <Text style={styles.message}>{message}</Text>
                 </View>
             </View>
         </TouchableHighlight>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
-    listrow: {
-        marginHorizontal: 5,
-        marginVertical: 1,
+    container: {
+        marginBottom: 20,
+        backgroundColor: '#FFF',
+        borderRadius: 10,
+        overflow: 'hidden',
+    },
+    header: {
         flexDirection: 'row',
-        elevation: 1,
-        borderRadius: 2
+        alignItems: 'center',
+        padding: 10,
     },
     avatar: {
-        margin: 10,
-        height: 100,
-        width: 100
+        height: 40,
+        width: 40,
+        borderRadius: 20,
+        marginRight: 10,
     },
-    info: {
-        flexDirection: 'column',
-        justifyContent: 'center'
+    owner: {
+        fontWeight: 'bold',
+        fontSize: 16,
     },
-    name: {
+    image: {
+        width: '100%',
+        height: 300,
+    },
+    content: {
+        padding: 10,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
         marginBottom: 5,
-        fontSize: 25,
-        fontWeight: 'bold'
     },
-    id: {
-        marginBottom: 5,
-        fontSize: 20
-    }
+    message: {
+        fontSize: 16,
+        color: '#444',
+    },
 });
 
 export default PostListRow;
